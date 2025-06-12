8b44a229e64aeebb5f0f3de72ed96090ece47caf
mafiasyahadatain <65263812+mafiasyahadatain@users.noreply.github.com>
2025-06-12 20:27:58 +0700

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -28,11 +28,8 @@ class RouteServiceProvider extends Provider
     {
         $this->app->singleton(Router::class);
         if (!Route::setRouteFromCacheIfExist()) {
-            // Sementara kita disable CSRF middleware:
-            Route::group(function () {
-                Route::setRouteFromFile();
-            });
-
+            // Hapus CSRF sementara:
+            Route::setRouteFromFile();
 
             Route::prefix(static::$API_PREFIX)->middleware(CookieMiddleware::class)->group(function () {
                 Route::get('/health', [static::class, 'health']);
